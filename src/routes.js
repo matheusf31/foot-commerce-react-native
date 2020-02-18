fe71cca2b8be0429926e75c4358df52adfc078db
Matheus Filipe <mfcr031@gmail.com>
2020-02-18 09:48:01 -0300

    start

---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/Main';
+import Cart from './pages/Cart';
+
+import Header from './components/Header';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ header: Header }}>
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{ title: 'produtos' }}
+        />
+        <Stack.Screen name="Cart" component={Cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
