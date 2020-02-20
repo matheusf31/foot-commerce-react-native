72f9957d9721c99f64ce27bfb37724e722260ba9
Matheus Filipe <mfcr031@gmail.com>
2020-02-20 12:40:50 -0300

    arrumando o header da aplicação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-
-// import { connect } from 'react-redux';
-
+import React, { useState, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Wrapper, Container, Logo, BasketContainer, ItemCount } from './styles';
+import store from '../../store';
 
 export default function Header({ navigation }) {
-  console.tron.log(navigation);
+  const [product, setProduct] = useState(store.getState());
+
+  function updateStateFromStore() {
+    const currentState = store.getState();
+
+    if (product !== currentState) {
+      setProduct(currentState);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribeStore = store.subscribe(updateStateFromStore);
+
+    return () => unsubscribeStore();
+  });
 
   return (
     <Wrapper>
@@ -14,7 +26,7 @@ export default function Header({ navigation }) {
         <Logo />
         <BasketContainer onPress={() => navigation.navigate('Cart')}>
           <Icon name="shopping-basket" color="#FFF" size={24} />
-          <ItemCount>3</ItemCount>
+          <ItemCount>{product.cart.length}</ItemCount>
         </BasketContainer>
       </Container>
     </Wrapper>
