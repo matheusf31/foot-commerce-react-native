30bd8307ae4ab21f86869f138a65a51e038b551b
Matheus Filipe <mfcr031@gmail.com>
2020-02-23 00:19:19 -0300

    desafio 7 completo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './config/ReactotronConfig';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import NavigationService from './services/navigation';
 import Routes from './routes';
 
 export default function App() {
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -34,13 +34,13 @@ import {
   EmptyText,
 } from './styles';
 
-function Cart({ cart, removeFromCart, updateAmount, total }) {
+function Cart({ cart, removeFromCart, updateAmountRequest, total }) {
   function increment(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   }
 
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   }
 
   return (
