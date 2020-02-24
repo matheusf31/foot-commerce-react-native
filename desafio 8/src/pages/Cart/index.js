ceb6eb6e2c2b06d666fe9e59921a4b4e5d215ea6
Matheus Filipe <mfcr031@gmail.com>
2020-02-24 12:21:05 -0300

    config readme

--- desafio 8/src/pages/Cart/index.js
+++ desafio 8/src/pages/Cart/index.js
@@ -1,15 +1,9 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import * as CartActions from '../../store/modules/cart/actions';
-
 import colors from '../../styles/colors';
 import { formatPrice } from '../../util/format';
-
-import api from '../../services/api';
 
 import {
   Container,
@@ -34,13 +28,30 @@ import {
   EmptyText,
 } from './styles';
 
-function Cart({ cart, removeFromCart, updateAmountRequest, total }) {
+export default function Cart() {
+  const total = useSelector(state =>
+    formatPrice(
+      state.cart.reduce((totalSum, product) => {
+        return totalSum + product.price * product.amount;
+      }, 0)
+    )
+  );
+
+  const cart = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: formatPrice(product.price * product.amount),
+    }))
+  );
+
+  const dispatch = useDispatch();
+
   function increment(product) {
-    updateAmountRequest(product.id, product.amount + 1);
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount + 1));
   }
 
   function decrement(product) {
-    updateAmountRequest(product.id, product.amount - 1);
+    dispatch(CartActions.updateAmountRequest(product.id, product.amount - 1));
   }
 
   return (
@@ -56,7 +67,11 @@ function Cart({ cart, removeFromCart, updateAmountRequest, total }) {
                     <ProductTitle>{product.title}</ProductTitle>
                     <ProductPrice>{product.priceFormatted}</ProductPrice>
                   </ProductDetails>
-                  <ProductDelete onPress={() => removeFromCart(product.id)}>
+                  <ProductDelete
+                    onPress={() =>
+                      dispatch(CartActions.removeFromCart(product.id))
+                    }
+                  >
                     <Icon
                       name="delete-forever"
                       size={24}
@@ -102,22 +117,3 @@ function Cart({ cart, removeFromCart, updateAmountRequest, total }) {
     </Container>
   );
 }
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-// o cálculo do subtotal só vai ser feito se algo no reducer atualizar
-const mapStateToProps = state => ({
-  // state.cart => nome do reducer
-  cart: state.cart.map(product => ({
-    ...product,
-    subtotal: formatPrice(product.price * product.amount),
-  })),
-  total: formatPrice(
-    state.cart.reduce((total, product) => {
-      return total + product.price * product.amount;
-    }, 0)
-  ),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cart);
